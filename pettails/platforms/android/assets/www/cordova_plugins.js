01c473415b8c0c4476ce5d88bca441e137749216
Alexander Michael Roche <alexandermichaelroche@googlemail.com>
2014-10-12 02:47:11 +0100

    Got me some motherfuckin' plugins!

--- pettails/platforms/android/assets/www/cordova_plugins.js
+++ pettails/platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,25 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.oracle.mx.ux.cordova.estimotebeacons/www/EstimoteBeacons.js",
+        "id": "com.oracle.mx.ux.cordova.estimotebeacons.EstimoteBeacons",
+        "clobbers": [
+            "EstimoteBeacons"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "com.oracle.mx.ux.cordova.estimotebeacons": "0.0.4",
+    "org.apache.cordova.splashscreen": "0.3.5-dev"
+}
 // BOTTOM OF METADATA
 });
