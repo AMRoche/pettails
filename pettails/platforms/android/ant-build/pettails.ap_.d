c0f95a6cede342d614dd8a421b596d7ee82940c6
Alexander Michael Roche <alexandermichaelroche@googlemail.com>
2014-10-12 03:56:10 +0100

    Got me a splashscreen and an icon!

--- pettails/platforms/android/ant-build/pettails.ap_.d
+++ pettails/platforms/android/ant-build/pettails.ap_.d
@@ -2,6 +2,10 @@ C:\xampp\htdocs\pettails\pettails\platforms\android\ant-build\pettails.ap_ \
  : C:\xampp\htdocs\pettails\pettails\platforms\android\res\drawable\dog_chasing_tail.jpg \
 C:\xampp\htdocs\pettails\pettails\platforms\android\res\drawable\dog_chasing_tail_splash.png \
 C:\xampp\htdocs\pettails\pettails\platforms\android\res\drawable\icon.png \
+C:\xampp\htdocs\pettails\pettails\platforms\android\res\drawable-hdpi\icon.png \
+C:\xampp\htdocs\pettails\pettails\platforms\android\res\drawable-ldpi\icon.png \
+C:\xampp\htdocs\pettails\pettails\platforms\android\res\drawable-mdpi\icon.png \
+C:\xampp\htdocs\pettails\pettails\platforms\android\res\drawable-xhdpi\icon.png \
 C:\xampp\htdocs\pettails\pettails\platforms\android\res\values\strings.xml \
 C:\xampp\htdocs\pettails\pettails\platforms\android\res\xml\config.xml \
 C:\xampp\htdocs\pettails\pettails\platforms\android\ant-build\res\drawable\dog_chasing_tail_splash.png \
